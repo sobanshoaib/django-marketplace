3dab6b63ca2e252096319bf36fc81c734f0b93df
Soban Shoaib <sobanshoaib7500@gmail.com>
2025-01-06 18:15:37 -0700

    added logic to delete and edit and other things

--- coolMarketplace/coolMarketplace/urls.py
+++ coolMarketplace/coolMarketplace/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
